8d1cff4189fc3a3d42851443c0bb3211aa5ecb31
peyman iravani <peyman.iravani@gmail.com>
2025-03-09 18:28:08 +0100

    update - worked on DB

--- src/core/DBClient.js
+++ src/core/DBClient.js
@@ -1,22 +1,39 @@
-const mysql = require("mysql2/promise");
+const { Pool } = require("pg");
 require("dotenv").config();
+const Logger = require("../../utils/logger");
+const fs = require("fs");
 
 class DBClient {
     constructor() {
-        this.pool = mysql.createPool({
+        this.pool = new Pool({
             host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
             database: process.env.DB_NAME,
+            ssl: {
+                rejectUnauthorized: false,
+                ca: fs.readFileSync(process.env.SSL_CA_FILE).toString(),
+                key: fs.readFileSync(process.env.SSL_KEY_FILE).toString(),
+                cert: fs.readFileSync(process.env.SSL_CERT_FILE).toString(),
+            },
+            statement_timeout: 5000, // 5 seconds timeout for queries
+            ssl: { rejectUnauthorized: false },
+            application_name: "Falcon-Automation",
         });
     }
 
     async query(sql, params = []) {
-        console.log(`🔹 Running DB Query: ${sql}`);
-        const connection = await this.pool.getConnection();
-        const [rows] = await connection.execute(sql, params);
-        connection.release();
-        return rows;
+        try {
+            Logger.info(`🔹 Running DB Query: ${sql}`);
+            const client = await this.pool.connect();
+            const res = await client.query(sql, params);
+            client.release();
+            return res.rows;
+        } catch (error) {
+            Logger.error(`❌ Database Query Failed: ${error.message}`);
+            throw error;
+        }
     }
 }
 
